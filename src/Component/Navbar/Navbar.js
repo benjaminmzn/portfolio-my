d867345c85168ad988b6fdc86fc4420e9021e076
Benjamin Mazoin <benjamin.mazoin.pro@gmail.com>
2022-08-08 16:02:19 +0200

    Added react application with navbar and sidebar empty components

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+function Navbar(){
+    return(
+        <div class="navbar-body">
+            <ul class="navbar-options">
+                <li>FR</li>
+                <li>EN</li>
+                <li>☀</li>
+                <li>🌙</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/Component/Sidebar/Sidebar.js
@@ -0,0 +1,18 @@
+import './Sidebar.css';
+
+function Sidebar(){
+    return(
+        <div class="sidebar-body">
+            <div class="sidebar-logo"></div>
+            <li class="sidebar-routes">
+                <ul>Home</ul>
+                <ul>Scholarship</ul>
+                <ul>Career</ul>
+                <ul>Contact</ul>
+            </li>
+
+        </div>
+    );
+}
+
+export default Sidebar;
