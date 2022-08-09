caaea75d274ea23ef2f8050447d759e10f7740e7
Benjamin Mazoin <benjamin.mazoin.pro@gmail.com>
2022-08-09 17:57:08 +0200

    Implemented navbar and added icons for light/dark mode

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,13 +1,15 @@
 import './Navbar.css';
+import whitemoonicon from '../../Assets/white-moon-icon.svg';
+import whitesunicon from '../../Assets/white-sun-icon.svg';
 
 function Navbar(){
     return(
         <div class="navbar-body">
             <ul class="navbar-options">
-                <li>FR</li>
-                <li>EN</li>
-                <li>☀</li>
-                <li>🌙</li>
+                <li class="navbar-options-li-text">FR</li>
+                <li class="navbar-options-li-text">EN</li>
+                <li class="navbar-options-li-icon"><img src={whitemoonicon} alt="DarkModeIcon" width="20" height="20"/></li>
+                <li class="navbar-options-li-icon"><img src={whitesunicon} alt="LightModeIcon" width="20" height="20"/></li>
             </ul>
         </div>
     );
